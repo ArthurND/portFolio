6e59e48766d4e590446c9e87db3734d4cd849d10
BILEG <>
2023-01-09 14:15:47 +0100

    skills

--- main.js
+++ main.js
@@ -18,15 +18,29 @@ if (navClose) {
   });
 }
 // ========= remove menu mobile =========
-const navLink = document.getElementsByClassName("nav__link");
-
+const navLink = document.querySelectorAll(".nav__link");
 function linkAction() {
   const navMenu = document.getElementById("nav-menu");
-  console.log(navLink);
-  console.log(navMenu);
+
   //when we click on each nav__link , we remove the show menu class
   navMenu.classList.remove("show-menu");
 }
 
-navLink.forEach((n) => n.addEventListener("click", linkAction()));
-// il y a une erreur sur la photo du dessus mais je ne vois pas laquelle
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+// ========== accoreon skills ==========
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
